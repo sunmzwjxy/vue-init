e91daf0be0139ec797161360ff6b31b293076331
sunmzwjxy <sunmz_wjxy@163.com>
2020-06-18 18:22:17 +0800

    comment for ES6 and Node for import and export

--- src/main.js
+++ src/main.js
@@ -5,3 +5,14 @@ var vm = new Vue({
     el: '#app',
     render: f => f(App)
 })
+
+
+/*
+    ES6
+    import ... from ''
+    export default and export
+
+    Node
+    var ... = require('')
+    module.exports & exports
+ */
